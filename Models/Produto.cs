e965a7129b1dfd100248e027f8541442bbca422c
Arthur Lopes <arthurestak@gmail.com>
2025-11-19 19:52:41 -0300

    Atualização final deste projeto de teste

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -7,9 +7,11 @@ namespace ProjetoInicial.Models
         [Required(ErrorMessage = "Campo obrigatório!")]
         public String nome { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [Range(0.1,1000000, ErrorMessage = "Digite um preço válido!")]
         public double preco { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [Range(1, 1000000, ErrorMessage = "Digite uma quantidade válida!")]
         public int quantidade { get; set; }
     }
