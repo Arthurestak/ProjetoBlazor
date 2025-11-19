79bfedee5ca4c5fe41d90fe25e3a413938ff31a7
Arthur Lopes Oliveira <arthur.lo@dinfor.uniceub>
2025-11-19 12:38:03 -0300

    Atualização geral

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoInicial.Models
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public string email { get; set; }
+        public DateOnly dataNascimento { get; set; }
+        public string telefone { get; set; }
+        public string cpf { get; set; }
+
+    }
+}
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,9 +1,16 @@
-﻿namespace ProjetoInicial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoInicial.Models
 {
     public class Produto
     {
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public String nome { get; set; }
+
+        [Range(0.1,1000000, ErrorMessage = "Digite um preço válido!")]
         public double preco { get; set; }
+
+        [Range(1, 1000000, ErrorMessage = "Digite uma quantidade válida!")]
         public int quantidade { get; set; }
     }
 }
