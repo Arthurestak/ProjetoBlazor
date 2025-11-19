0470ab08e1192c74b9c410fae9fbb4d26d2c6c48
Arthur Lopes Oliveira <arthur.lo@dinfor.uniceub>
2025-11-19 09:59:31 -0300

    Atualização tela Cadastrar Produtos

---
+++ Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoInicial.Models
+{
+    public class Produto
+    {
+        public String nome { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
